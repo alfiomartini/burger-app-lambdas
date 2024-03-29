b041ae11ea2a6f2a34729bdd77c764741cd082d1
Alfio Martini <alfio.martini@encora.com>
2024-03-29 16:56:53 -0300

    fix cors with headers - ingredientsDeleteId

--- ingredientsDeleteId/src/index.mjs
+++ ingredientsDeleteId/src/index.mjs
@@ -1,21 +1,29 @@
 import { connection } from "./client.mjs";
 
 export const handler = async (event) => {
-  let response = {};
+  let response = {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
   try {
     const id = event["path"].split("/")[2];
     await connection.query("delete from ingredient where ing_id = ?", [id]);
     // test if results is not empty
     response = {
+      ...response,
       statusCode: 200,
       body: JSON.stringify(`Record successfully deleted`),
     };
   } catch (error) {
     console.log("delete ingredient/id", error);
     response = {
+      ...response,
       statusCode: 500,
       body: JSON.stringify(`Internal server error`),
     };
   }
+  console.log(response);
   return response;
 };
